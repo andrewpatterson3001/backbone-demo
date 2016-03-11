266fdec4d4e7df67e89d4e77fd651970cedae133
Andrew Patterson <andrew.patterson.3001@gmail.com>
2016-03-11 14:45:33 -0800

    working event to add message when submitting a message

--- views/app-view.js
+++ views/app-view.js
@@ -1,16 +1,19 @@
 var AppView = Backbone.View.extend({
   el: $("body"),
   events: {
-    'keypress': 'sayHello'
+    'click #submit-button': 'sayHello'
   },
   initialize: function() {
     alert('initializing')
   },
   render: function(){
     var testString = 'hello world'
-    this.$el.append(testString);
+    var messageSays = $('#add-new-message').val()
+    this.$el.append($('<p>').text(messageSays));
+    this.$('#add-new-message').val('');
   },
-  sayHello: function(){
+  sayHello: function(event){
+    event.preventDefault();
     this.render();
   }
 });
